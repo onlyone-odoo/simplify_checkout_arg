8e9652d540b3a71d6e9ca555cbe4e03530c4d431
onlyone-odoo <matiasb@onlyone.com.ar>
2025-02-18 10:26:12 -0300

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Simplify Checkout Argentina",
+    "summary": "Simplifica el checkout estableciendo valores por defecto",
+    "description": "Módulo para establecer valores por defecto en el checkout del ecommerce usando solo vistas.",
+    "category": "Website",
+    "depends": ["website_sale", "l10n_ar_website_sale"],
+    "data": [
+        "views/website_sale_views.xml",
+    ],
+    "author": "Be OnlyOne",
+    "maintainers": ["onlyone-odoo"],
+    "website": "https://onlyone.odoo.com/",
+    "license": "AGPL-3",
+    "category": "Technical Settings",
+    "version": "17.0.1.0.0",
+    "development_status": "Production/Stable",
+    "application": False,
+    "installable": True,
+}
